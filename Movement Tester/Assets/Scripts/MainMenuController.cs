19f900fcd250ccf32d5a5011e51617206b83ebcb
SerinHan35 <serkanarak35@gmail.com>
2026-04-14 08:56:02 +0100

    Main Menu Done

---
+++ Movement Tester/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private string firstLevelSceneName = "HealthSystemTest";
+    [SerializeField] private string creditsSceneName = "Credits";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevelSceneName);
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(creditsSceneName);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game pressed");
+
+        Application.Quit();
+    }
+}
